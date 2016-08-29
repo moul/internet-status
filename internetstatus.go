5b651ce7e068547922a6a5372b14c39d2afe6070
Manfred Touron <m@42.am>
2016-08-29 11:16:06 +0200

    Add Quick helper placeholder

--- internetstatus.go
+++ internetstatus.go
@@ -4,15 +4,23 @@ import (
 	"github.com/moul/internet-status/pkg/dns"
 )
 
+// Result contains checks returns
 type Result struct {
 	QueryRootDNSA    error
 	QueryRootDNSAAAA error
 }
 
+// Full performs all the checks to determine the connectivity status (most accurate)
 func Full() (Result, error) {
 	result := Result{
 		QueryRootDNSA:    internetstatus_dns.QueryRootDNSA(),
 		QueryRootDNSAAAA: internetstatus_dns.QueryRootDNSAAAA(),
 	}
+	return result, nil
+}
+
+// Quick performs minimal checks to determine the connectivity status (less accurate)
+func Quick() (Result, error) {
+	result := Result{}
 	return result, nil
 }
