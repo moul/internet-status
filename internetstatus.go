5ec624bce994629740c80f06b6c4bab3c7f27881
Manfred Touron <m@42.am>
2016-08-29 10:57:27 +0200

    Testing QueryRootDNS A and AAAA

--- internetstatus.go
+++ internetstatus.go
@@ -1,9 +1,18 @@
 package internetstatus
 
+import (
+	"github.com/moul/internet-status/pkg/dns"
+)
+
 type Result struct {
+	QueryRootDNSA    error
+	QueryRootDNSAAAA error
 }
 
 func Full() (Result, error) {
-	result := Result{}
+	result := Result{
+		QueryRootDNSA:    internetstatus_dns.QueryRootDNSA(),
+		QueryRootDNSAAAA: internetstatus_dns.QueryRootDNSAAAA(),
+	}
 	return result, nil
 }
