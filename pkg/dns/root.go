95fe242168eecf7c62143c23635b36e54845ece3
Manfred Touron <m@42.am>
2016-08-30 20:30:38 +0200

    Remove logging

--- pkg/dns/root.go
+++ pkg/dns/root.go
@@ -2,7 +2,6 @@ package internetstatus_dns
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/miekg/dns"
 	"github.com/moul/internet-status/pkg/root-hints"
@@ -14,8 +13,7 @@ func QueryRootDNSA() error {
 	req.Question = make([]dns.Question, 1)
 	req.Question[0] = dns.Question{".", dns.TypeNS, dns.ClassINET}
 	server := roothints.RandomA()
-	in, err := dns.Exchange(req, fmt.Sprintf("%s:53", server.A))
-	log.Printf("QueryRootDNSA: in=%v, err=%v", in, err)
+	_, err := dns.Exchange(req, fmt.Sprintf("%s:53", server.A))
 	return err
 }
 
@@ -25,7 +23,6 @@ func QueryRootDNSAAAA() error {
 	req.Question = make([]dns.Question, 1)
 	req.Question[0] = dns.Question{".", dns.TypeNS, dns.ClassINET}
 	server := roothints.RandomAAAA()
-	in, err := dns.Exchange(req, fmt.Sprintf("[%s]:53", server.AAAA))
-	log.Printf("QueryRootDNSA: in=%v, err=%v", in, err)
+	_, err := dns.Exchange(req, fmt.Sprintf("[%s]:53", server.AAAA))
 	return err
 }
