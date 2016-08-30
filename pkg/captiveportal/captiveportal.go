92d84569a712e7550ebc0d49f030eb0f10db526a
Manfred Touron <m@42.am>
2016-08-30 20:09:07 +0200

    Reduce timeout on captive portal

--- pkg/captiveportal/captiveportal.go
+++ pkg/captiveportal/captiveportal.go
@@ -4,23 +4,33 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 const expectedBody = "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</BODY></HTML>"
 
 // Check checks for the presence of captive portal
 func Check() error {
-	resp, err := http.Get("http://www.apple.com/library/test/success.html")
+	// configure HTTP client
+	timeout := time.Duration(2 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	// perform HTTP request
+	resp, err := client.Get("http://www.apple.com/library/test/success.html")
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
+	// read the Body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
 
+	// check for expected body
 	if string(body) != expectedBody {
 		return fmt.Errorf("body differs from expected result")
 	}
